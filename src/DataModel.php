7048e719720a7b5dc05db3f3a0f245a516a320ea
Joe Maller <joe@joemaller.com>
2020-05-05 17:52:06 -0400

    add a plugin page error fixes

--- src/DataModel.php
+++ src/DataModel.php
@@ -25,7 +25,7 @@ abstract class DataModel
          */
         add_action('plugins_loaded', [$this, 'register']);
 
-        add_filter('pre_set_site_transient_update_plugins', [$this, 'update']);
+        add_filter('pre_set_site_transient_update_plugins', [$this, 'update'], 10, 2);
         add_filter('plugins_api', [$this, 'details'], 10, 3);
 
         add_action('admin_enqueue_scripts', [$this, 'adminStyles'], 100);
@@ -42,6 +42,12 @@ abstract class DataModel
     /**
      * These `get_plugin_data` and `plugin_basename` functions are not available
      * until late in the init hook.
+     *
+     * The set values look like this:
+     *   $this->plugin_info = See https://developer.wordpress.org/reference/functions/get_plugin_data/
+     *   $this->plugin_id = "njhi-data-model/main.php"
+     *   $this->plugin_slug = "njhi-data-model"
+     *
      */
     public function getInfo()
     {
@@ -57,6 +63,9 @@ abstract class DataModel
         $this->getInfo();
         $response = get_transient($this->transient);
 
+        /**
+         * Disable transients when WP_DEBUG is true
+         */
         if (WP_DEBUG === true) {
             $response = false;
         }
@@ -66,6 +75,7 @@ abstract class DataModel
                 'body' => json_encode([
                     'version' => $this->plugin_info['Version'],
                     'slug' => $this->plugin_slug,
+                    'plugin' => $this->plugin_id,
                 ]),
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -80,72 +90,28 @@ abstract class DataModel
                 echo "Something went wrong: $error_message";
             } else {
                 /**
-                 * WordPress expects $response to be an object with all internal keys being arrays
-                 * So, json_decode to an associative array, then cast it to an object later on
+                 * WordPress expects $response to be an object with all internal keys
+                 * being arrays.
+                 * Quick solution: json_decode to an associative array, then cast it
+                 * to an object later so all top-level keys become properties.
                  */
                 $response = json_decode($remote['body'], true);
+
                 set_transient($this->transient, $response, 24 * HOUR_IN_SECONDS);
             }
         }
         return (object) $response;
     }
 
-    public function update($transient)
+    public function update($transient, $action)
     {
-        /**
-         * These `get_plugin_data` and `plugin_basename` functions are not available
-         * until late in the init hook. The're only used by the update method, so
-         * there's no need to move them out of here.
-         */
-        // $this->plugin_info = get_plugin_data($this->childFilePath);
-        // $this->plugin_id = plugin_basename($this->childFilePath);
-        // $this->plugin_slug = dirname($this->plugin_id);
-
         $this->getInfo();
-        // $this->transient = "ideasonpurpose-update-check_{$this->plugin_id}";
-        // $response = get_transient($this->transient);
-
-        // if (WP_DEBUG === true) {
-        //     $response = false;
-        // }
-
-        // if ($response === false) {
-        //     $remote = wp_remote_post('https://1q32dgotuh.execute-api.us-east-2.amazonaws.com/production', [
-        //         'body' => json_encode([
-        //             'version' => $this->plugin_info['Version'],
-        //             'slug' => $this->plugin_slug,
-        //         ]),
-        //         'headers' => [
-        //             'Content-Type' => 'application/json',
-        //             'Accept' => 'application/json',
-        //         ],
-        //         'timeout' => 10,
-        //         'data_format' => 'body',
-        //     ]);
-
-        //     if (is_wp_error($response)) {
-        //         $error_message = $response->get_error_message();
-        //         echo "Something went wrong: $error_message";
-        //     } else {
-        //         /**
-        //          * WordPress expects $response to be an object with all internal keys being arrays
-        //          * So, json_decode to an associative array, then cast it to an object later on
-        //          */
-        //         $response = json_decode($remote['body'], true);
-        //         set_transient($this->transient, $response, 24 * HOUR_IN_SECONDS);
-        //     }
-        // }
 
         $response = $this->updateCheck();
 
         if ((array) $response) {
             $transient->response[$this->plugin_id] = (object) $response;
         }
-
-        \Kint::$mode_default = \Kint::MODE_CLI;
-        error_log(@d($transient->response));
-
-        \Kint::$mode_default = \Kint::MODE_RICH;
 
         return $transient;
     }
@@ -155,15 +121,10 @@ abstract class DataModel
         $this->getInfo();
         $this->response = $this->updateCheck();
 
-        \Kint::$mode_default = \Kint::MODE_CLI;
-        error_log(@d($result, $action, $args));
-
-        \Kint::$mode_default = \Kint::MODE_RICH;
-
-        if (empty($args->slug) || $args->slug != $this->plugin_slug) {
-            return false;
+        if ($action !== 'plugin_information') {
+            return $result;
         }
-        //  $response = clone $args;
+
         $response = new \StdClass();
         $response->slug = $this->plugin_slug;
         $response->name = $this->plugin_info['Name'];
@@ -182,10 +143,6 @@ abstract class DataModel
             'About' => 'About Page',
         ];
 
-        \Kint::$mode_default = \Kint::MODE_CLI;
-        error_log(@d($this->plugin_info, $this->plugin_id, $this->plugin_slug, $result, $action, $args, $response));
-
-        \Kint::$mode_default = \Kint::MODE_RICH;
         return $response;
     }
 
