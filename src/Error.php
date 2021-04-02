176b0797e4799b55fe0b462b9825011ecfd028f5
Joe Maller <joe@joemaller.com>
2021-04-02 15:06:40 -0600

    refactor Error handler

--- src/Error.php
+++ src/Error.php
@@ -1,8 +1,31 @@
 <?php namespace IdeasOnPurpose\WP;
 
+/**
+ * A basic error handler for WordPress Projects.
+ * Messages will be always be logged with PHP's error_log function.
+ * If the WP_DEBUG constant is set and true, an HTML comment will also
+ * be added to the document from the wp_head hook.
+ *
+ * Log an error like this:
+ *
+ *    new WP\Error('error message');
+ *
+ * @link https://www.php.net/manual/en/function.error-log.php
+ * @link https://developer.wordpress.org/reference/hooks/wp_head/
+ *
+ * TODO: Migrate this to it's own project (more portable & consistent)
+ * TODO: Any reason not to just wrap Monolog? Why re-invent the wheel?
+ *       Or, too bloated? We only need a basic log and head-injection
+ *       @link http://seldaek.github.io/monolog/
+ */
 class Error
 {
     public function __construct($msg)
+    {
+        $this->handler($msg);
+    }
+
+    public function handler($msg)
     {
         $this->is_debug = defined('WP_DEBUG') && WP_DEBUG;
         $trace = debug_backtrace();
